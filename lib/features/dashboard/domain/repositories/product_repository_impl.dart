60db7557f4df650d1f45cf992b4f2ceb87e1a182
Frientia <muhamadyajidky@gmail.com>
2026-04-14 14:30:34 +0700

    feat: model_repository_impl.dart
    menambahkan model repo impl

---
+++ lib/features/dashboard/domain/repositories/product_repository_impl.dart
@@ -0,0 +1,34 @@
+ 
+import 'package:book_store/core/constants/api_constants.dart';
+import 'package:book_store/core/services/dio_client.dart';
+import 'package:book_store/features/dashboard/data/models/product_model.dart';
+import 'package:book_store/features/dashboard/domain/repositories/product_repository.dart';
+
+class ProductRepositoryImpl implements ProductRepository { 
+  @override 
+  Future<List<ProductModel>> getProducts({ 
+    int page = 1, 
+    int limit = 10, 
+    String? category, 
+  }) async { 
+    final response = await DioClient.instance.get( 
+      ApiConstants.products, 
+      queryParameters: { 
+        'page': page, 
+        'limit': limit, 
+        'category': category, 
+      }, 
+    ); 
+ 
+    final List<dynamic> data = response.data['data']; 
+    return data.map((e) => ProductModel.fromJson(e)).toList(); 
+  } 
+ 
+  @override 
+  Future<ProductModel> getProductById(int id) async { 
+    final response = await DioClient.instance.get( 
+      '${ApiConstants.products}/$id', 
+    ); 
+    return ProductModel.fromJson(response.data['data']); 
+  } 
+}
