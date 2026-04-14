c17812b9b61b9beca0a6708f1c70193f9282837b
Frientia <muhamadyajidky@gmail.com>
2026-04-14 13:26:45 +0700

    feat: auth_provider.dart
    menambahkan class authprovider

--- lib/features/auth/data/presentation/providers/auth_provider.dart
+++ lib/features/auth/data/presentation/providers/auth_provider.dart
@@ -7,3 +7,21 @@ enum AuthStatus {
   emailNotVerified, // Login tapi email belum dikonfirmasi
   error,            // Ada error
 }
+
+class AuthProvider extends ChangeNotifier { 
+  final FirebaseAuth _auth = FirebaseAuth.instance; 
+  final GoogleSignIn _googleSignIn = GoogleSignIn(); 
+ 
+  // ─── State ─────────────────────────────────────────────── 
+  AuthStatus _status = AuthStatus.initial; 
+  User?     _firebaseUser; 
+  String?   _backendToken;   // Token dari backend (bukan Firebase token) 
+  String?   _errorMessage; 
+ 
+  AuthStatus get status       => _status; 
+  User?      get firebaseUser  => _firebaseUser; 
+  String?    get backendToken  => _backendToken; 
+  String?    get errorMessage  => _errorMessage; 
+  bool       get isLoading     => _status == AuthStatus.loading;  
+  
+} 
