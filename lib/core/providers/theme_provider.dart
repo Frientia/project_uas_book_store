ca11673984db33437c67720e739975a4fca201f8
Frientia <muhamadyajidky@gmail.com>
2026-06-16 13:02:24 +0700

    feat(theme): add persistence to theme preference
    implementasi shared_preferences ke Theme Provider

--- lib/core/providers/theme_provider.dart
+++ lib/core/providers/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDark = false;
@@ -7,8 +8,20 @@ class ThemeProvider extends ChangeNotifier {
 
   ThemeMode get themeMode => _isDark ? ThemeMode.dark : ThemeMode.light;
 
-  void toggle() {
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool('isDark') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggle() async {
     _isDark = !_isDark;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', _isDark);
   }
 }
